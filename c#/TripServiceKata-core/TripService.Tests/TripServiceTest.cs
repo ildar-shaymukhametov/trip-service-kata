2c4a7f3540521c5e3dc9271c1450e4b6d8d9ce89
Шаймухаметов Ильдар <shaimildar@gmail.com>
2021-08-11 22:47:18 +0500

    add net core version

---
+++ c#/TripServiceKata-core/TripService.Tests/TripServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using ContosoTrips.Exceptions;
+using ContosoTrips.Trips;
+using ContosoTrips.Users;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace TripServiceTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
